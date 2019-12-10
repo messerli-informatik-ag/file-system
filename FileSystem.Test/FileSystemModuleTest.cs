d2ebd66ae8b2c6efe0ada241905dc9f127b103bd
Sergio Sutter <sergio.sutter@messerli.ch>
2019-12-10 15:13:54 +0100

    Extract FileSystemTest class definitions

---
+++ FileSystem.Test/FileSystemModuleTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Autofac;
+using Messerli.Test.Utility;
+using Xunit;
+
+namespace Messerli.FileSystem.Test
+{
+    public sealed class FileSystemModuleTest : IDisposable
+    {
+        private readonly IContainer _container =
+            new CompositionRootBuilder()
+                .RegisterModule(new FileSystemModule())
+                .Build();
+
+        [Theory]
+        [ClassData(typeof(ObjectArrayEnumerable<ModuleInterfaceEnumerable<FileSystemModule>>))]
+        public void ModuleCanBeResolved(Type type)
+        {
+            Assert.NotNull(_container.Resolve(type));
+        }
+
+        public void Dispose()
+        {
+            _container.Dispose();
+        }
+    }
+}
