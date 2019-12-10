dc3a44a93e5889baa06b6c5e0b9b23b8183533cd
Sergio Sutter <sergio.sutter@messerli.ch>
2019-12-10 15:06:04 +0100

    Extract FileSystem class definitions

---
+++ FileSystem/FileSystemModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace Messerli.FileSystem
+{
+    public sealed class FileSystemModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<FileSystem>().As<IFileSystem>();
+        }
+    }
+}
