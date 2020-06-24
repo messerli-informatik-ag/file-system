aa4b58e623f19628bc8bdf3ac8040be89d242683
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-06-24 16:58:16 +0200

    Document methods on IFileOpeningBuilder

--- FileSystem/IFileOpeningBuilder.cs
+++ FileSystem/IFileOpeningBuilder.cs
@@ -4,18 +4,39 @@ namespace Messerli.FileSystem
 {
     public interface IFileOpeningBuilder
     {
+        /// <summary>
+        /// Specifies that the operating system should create a new file. If the file already exists, it will be overwritten.
+        /// </summary>
         IFileOpeningBuilder Create(bool create = true);
 
+        /// <summary>
+        /// Opens the file if it exists and seeks to the end of the file, or creates a new file.
+        /// Requires that <see cref="Write"/> is called to.
+        /// </summary>
         IFileOpeningBuilder Truncate(bool truncate = true);
 
+        /// <summary>
+        /// Opens the file if it exists and seeks to the end of the file, or creates a new file.
+        /// </summary>
         IFileOpeningBuilder Append(bool append = true);
 
+        /// <summary>
+        /// Write access to the file. Data can be written to the file. Combine with <see cref="Read"/> for read/write access.
+        /// </summary>
         IFileOpeningBuilder Write(bool write = true);
 
+        /// <summary>
+        /// Read access to the file. Data can be read from the file. Combine with <see cref="Write"/> for read/write access.
+        /// </summary>
         IFileOpeningBuilder Read(bool read = true);
 
+        /// <summary>
+        /// Specifies that the operating system should create a new file.
+        /// If the file already exists, an <see cref="IOException"/> exception is thrown.
+        /// </summary>
         IFileOpeningBuilder CreateNew(bool createNew = true);
 
+        /// <exception cref="IOException" />
         Stream Open(string path);
     }
 }
