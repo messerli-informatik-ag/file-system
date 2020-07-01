b47b21e9608b586b00ebdedab867c30617a01773
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-07-01 12:33:36 +0200

    Make sentences more consistent

--- FileSystem/IFileOpeningBuilder.cs
+++ FileSystem/IFileOpeningBuilder.cs
@@ -5,12 +5,12 @@ namespace Messerli.FileSystem
     public interface IFileOpeningBuilder
     {
         /// <summary>
-        /// Specifies that the operating system should create a new file. If the file already exists, it will be overwritten.
+        /// Specifies that <see cref="Open"/> should create a new file. If the file already exists, it will be overwritten.
         /// </summary>
         IFileOpeningBuilder Create(bool create = true);
 
         /// <summary>
-        /// Specifies that the operating system should open an existing file. When the file is opened, it should be truncated so that its size is zero bytes.
+        /// Specifies that <see cref="Open"/> should open an existing file. When the file is opened, it should be truncated so that its size is zero bytes.
         /// Requires that <see cref="Write"/> is called too. Can not be used to together with <see cref="Append"/>.
         /// </summary>
         IFileOpeningBuilder Truncate(bool truncate = true);
@@ -22,18 +22,20 @@ namespace Messerli.FileSystem
         IFileOpeningBuilder Append(bool append = true);
 
         /// <summary>
-        /// Write access to the file. Data can be written to the file. Combine with <see cref="Read"/> for read/write access.
+        /// Specifies that <see cref="Open"/> should open the file with write access.
+        /// Data can be written to the file. Combine with <see cref="Read"/> for read/write access.
         /// </summary>
         IFileOpeningBuilder Write(bool write = true);
 
         /// <summary>
-        /// Read access to the file. Data can be read from the file. Combine with <see cref="Write"/> for read/write access.
+        /// Specifies that <see cref="Open"/> should open the file with read access.
+        /// Combine with <see cref="Write"/> for read/write access.
         /// </summary>
         IFileOpeningBuilder Read(bool read = true);
 
         /// <summary>
-        /// Specifies that the operating system should create a new file.
-        /// If the file already exists, an <see cref="IOException"/> exception is thrown when calling <see cref="Open"/>.
+        /// Specifies that <see cref="Open"/> should create a new file.
+        /// If the file already exists, an <see cref="IOException"/> exception is thrown.
         /// </summary>
         IFileOpeningBuilder CreateNew(bool createNew = true);
 
